db4bc985a3a1ace83886d2a7c51630b82ff20871
Danon003 <danielvigerin@yandex.ru>
2025-07-09 12:24:02 +0300

    Initial commit

---
+++ src/main/java/ru/danon/StudentToDo/StudentToDoApplication.java
@@ -0,0 +1,13 @@
+package ru.danon.StudentToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/danon/StudentToDo/StudentToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danon.StudentToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
